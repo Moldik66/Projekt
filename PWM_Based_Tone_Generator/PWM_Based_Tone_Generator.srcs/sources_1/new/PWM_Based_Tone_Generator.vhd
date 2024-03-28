3a1172c1c73babce4a07c933c9478a4e50075be1
Molák Dominik (247830) <247830@vutbr.cz>
2024-03-28 15:26:22 +0100

    Projekt_uvod

---
+++ PWM_Based_Tone_Generator/PWM_Based_Tone_Generator.srcs/sources_1/new/PWM_Based_Tone_Generator.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/28/2024 03:20:49 PM
+-- Design Name: 
+-- Module Name: PWM_Based_Tone_Generator - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity PWM_Based_Tone_Generator is
+    Port ( clk : in STD_LOGIC);
+end PWM_Based_Tone_Generator;
+
+architecture Behavioral of PWM_Based_Tone_Generator is
+
+begin
+
+--AHOOOOOOJ
+
+
+end Behavioral;
